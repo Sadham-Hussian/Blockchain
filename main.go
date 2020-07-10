4c241c731efb9ba3a2e376da2fdfd843d88948fa
Sadham-Hussian <sadhamhussian2020@gmail.com>
2020-07-10 22:40:43 +0530

    Add methods to construct Blockchain

--- main.go
+++ main.go
@@ -6,32 +6,59 @@ import (
 	"fmt"
 )
 
-// struct to maintain the main chain of the block chain
+// Blockchain : struct to maintain the main chain of the block chain
 type Blockchain struct {
 	blocks []*Block
 }
 
-// struct to define a block
+// Block : struct to define a block
 type Block struct {
-	hash     []byte
-	data     []byte
-	prevHash []byte
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
 }
 
-// method to compute the hash of the block with data and prevHash
-func (b *Block) computeHash() {
-	info := bytes.Join([][]byte{b.data, b.prevHash}, []byte{})
-	hash_generated := sha256.Sum256(info)
-	b.hash = hash_generated[:]
+// ComputeHash : method to compute the hash of the block with data and prevHash
+func (b *Block) ComputeHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hashGenerated := sha256.Sum256(info)
+	b.Hash = hashGenerated[:]
 }
 
-// method to create a new block
-func createBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte{data}, prevHash}
-	block.computeHash()
+// CreateBlock : method to create a new block
+func CreateBlock(data string, PrevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), PrevHash}
+	block.ComputeHash()
 	return block
 }
 
+// AddBlock : method to add new block to the chain
+func (chain *Blockchain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	newBlock := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, newBlock)
+}
+
+// Genesis : method to create the genesis block.
+// genesis block does not have PrevHash
+func Genesis() *Block {
+	return CreateBlock("genesis", []byte{})
+}
+
+// InitBlockchain : method to create a Blockchain
+func InitBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{Genesis()}}
+}
+
 func main() {
-	fmt.Println("Hello, World.")
+	chain := InitBlockchain()
+
+	chain.AddBlock("Alice pay 20 BTC to Bob")
+	chain.AddBlock("Bob pay 40 BTC to Charlie")
+
+	for _, blocks := range chain.blocks {
+		fmt.Printf("\n\nBlock data : %s", blocks.Data)
+		fmt.Printf("\nBlock Hash : %x", blocks.Hash)
+		fmt.Printf("\nPrev Block Hash : %x", blocks.PrevHash)
+	}
 }
