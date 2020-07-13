7f8cc967daa0ed6653780327625164de881ca6e8
Sadham-Hussian <sadhamhussian2020@gmail.com>
2020-07-13 21:52:44 +0530

    Configure BadgerDB to create a blockchain

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,13 +1,58 @@
 package blockchain
 
+import (
+	"fmt"
+
+	"github.com/dgraph-io/badger"
+)
+
+const (
+	dbPath = "./temp/blocks"
+)
+
 // Blockchain : struct to maintain the main chain of the block chain
 type Blockchain struct {
-	Blocks []*Block
+	LastHash []byte
+	Database *badger.DB
 }
 
 // InitBlockchain : method to create a Blockchain
 func InitBlockchain() *Blockchain {
-	return &Blockchain{[]*Block{Genesis()}}
+	var lastHash []byte
+
+	db, err := badger.Open(badger.DefaultOptions("/temp/blocks"))
+	Handle(err)
+
+	err = db.Update(func(txn *badger.Txn) error {
+		if _, err := txn.Get([]byte("lh")); err == badger.ErrKeyNotFound {
+			fmt.Println("No existing blockchain found")
+			genesis := Genesis()
+			fmt.Println("Genesis approved")
+
+			err = txn.Set(genesis.Hash, genesis.Serialize())
+			Handle(err)
+
+			err = txn.Set([]byte("lh"), genesis.Hash)
+
+			lastHash = genesis.Hash
+
+			return err
+		} else {
+			item, err := txn.Get([]byte("lh"))
+			Handle(err)
+
+			err = item.Value(func(val []byte) error {
+				lastHash = val
+				return err
+			})
+
+			return err
+		}
+	})
+	Handle(err)
+
+	blockchain := Blockchain{lastHash, db}
+	return &blockchain
 }
 
 // AddBlock : method to add new block to the chain
