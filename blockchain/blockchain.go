3b69e12ebc1923f4f35ac0246936046efbfbd3be
Sadham-Hussian <sadhamhussian2020@gmail.com>
2020-07-13 23:10:18 +0530

    Add functions to retrieve blocks from the Db

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -91,3 +91,29 @@ func (chain *Blockchain) AddBlock(data string) {
 
 	Handle(err)
 }
+
+// Iterator : It returns the current Blockchain
+func (chain *BlockchainIterator) Iterator() *BlockchainIterator {
+	iter := &BlockchainIterator{chain.CurrentHash, chain.Database}
+
+	return iter
+}
+
+// Next : function to iterate the blockchain in BadgerDB and return a block
+func (iter *BlockchainIterator) Next() *Block {
+	var block *Block
+
+	err := iter.Database.View(func(txn *badger.Txn) error {
+		item, err := txn.Get(iter.CurrentHash)
+		Handle(err)
+		err = item.Value(func(val []byte) error {
+			block = Deserialize(val)
+			return err
+		})
+		return err
+	})
+	Handle(err)
+	iter.CurrentHash = block.PrevHash
+
+	return block
+}
