e55f88e09e70b15c8a6f5284ff6db7a23fee6806
Sadham-Hussian <sadhamhussian2020@gmail.com>
2020-07-15 21:36:34 +0530

    Add function to create Coinbase transaction

--- blockchain/transactions.go
+++ blockchain/transactions.go
@@ -1,5 +1,12 @@
 package blockchain
 
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/gob"
+	"fmt"
+)
+
 // Transaction : struct to handle details of a transaction
 type Transaction struct {
 	ID      []byte
@@ -10,12 +17,40 @@ type Transaction struct {
 // TxInput : struct to handle transaction input of a transaction
 type TxInput struct {
 	ID  []byte
-	Sig []byte
 	Out int
+	Sig string
 }
 
 // TxOutput : struct to handle transaction output of a transaction
 type TxOutput struct {
 	Value  int
 	PubKey string
+}
+
+// SetID : function to set the transaction ID
+func (tx *Transaction) SetID() {
+	var encoded bytes.Buffer
+	var Hash [32]byte
+
+	encode := gob.NewEncoder(&encoded)
+	err := encode.Encode(tx)
+	Handle(err)
+
+	Hash = sha256.Sum256(encoded.Bytes())
+	tx.ID = Hash[:]
+}
+
+// CoinbaseTx : firsttx in the block. Miner collects the block reward.
+func CoinbaseTx(to string, data string) *Transaction {
+	if data == "" {
+		data = fmt.Sprintf("Coins to %s", to)
+	}
+
+	txin := TxInput{[]byte{}, -1, data}
+	txout := TxOutput{100, to}
+
+	tx := Transaction{nil, []TxInput{txin}, []TxOutput{txout}}
+	tx.SetID()
+
+	return &tx
 }
