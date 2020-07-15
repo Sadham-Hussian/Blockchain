74a54bb4419e17f3993f83f4a440192d8435b969
Sadham-Hussian <sadhamhussian2020@gmail.com>
2020-07-15 22:39:44 +0530

    Modify data to Transaction in Block struct

--- blockchain/block.go
+++ blockchain/block.go
@@ -2,21 +2,36 @@ package blockchain
 
 import (
 	"bytes"
+	"crypto/sha256"
 	"encoding/gob"
 	"log"
 )
 
 // Block : struct to define a block
 type Block struct {
-	Hash     []byte // Hash of the block
-	Data     []byte // Data inside the block
-	PrevHash []byte // Hash of the previous block
-	Nonce    int    // a number to be found to find the Hash of the block
+	Hash         []byte         // Hash of the block
+	Transactions []*Transaction // Transactions in the block
+	PrevHash     []byte         // Hash of the previous block
+	Nonce        int            // a number to be found to find the Hash of the block
+}
+
+// HashTransactions : Function to hash all the transaction in the block
+func (b *Block) HashTransactions() []byte {
+	var txHashes [][]byte
+	var txHash [32]byte
+
+	for _, tx := range b.Transactions {
+		txHashes = append(txHashes, tx.ID)
+	}
+
+	txHash = sha256.Sum256(bytes.Join(txHashes, []byte{}))
+
+	return txHash[:]
 }
 
 // CreateBlock : method to create a new block
-func CreateBlock(data string, PrevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), PrevHash, 0}
+func CreateBlock(txs []*Transaction, PrevHash []byte) *Block {
+	block := &Block{[]byte{}, txs, PrevHash, 0}
 	pow := NewProof(block)
 	nonce, hash := pow.Run()
 	block.Nonce = nonce
@@ -26,8 +41,8 @@ func CreateBlock(data string, PrevHash []byte) *Block {
 
 // Genesis : method to create the genesis block.
 // genesis block does not have PrevHash
-func Genesis() *Block {
-	return CreateBlock("genesis", []byte{})
+func Genesis(coinbase *Transaction) *Block {
+	return CreateBlock([]*Transaction{coinbase}, []byte{})
 }
 
 // Serialize : method to serialize the entire block to bytes to store the
