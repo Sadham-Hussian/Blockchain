e50ce16dd7f7e1391749217663a48b977ce2c945
Sadham-Hussian <sadhamhussian2020@gmail.com>
2020-07-19 21:51:35 +0530

    blockchain/tx: Modify the TxInput and TxOutput struct and add utility functions

--- blockchain/tx.go
+++ blockchain/tx.go
@@ -1,26 +1,49 @@
 package blockchain
 
+import (
+	"bytes"
+
+	"github.com/Sadham-Hussian/Blockchain/wallet"
+)
+
 // TxInput : struct to handle transaction input of a transaction
 type TxInput struct {
-	ID  []byte
-	Out int
-	Sig string
+	ID        []byte
+	Out       int
+	Signature []byte
+	PubKey    []byte
 }
 
 // TxOutput : struct to handle transaction output of a transaction
 type TxOutput struct {
-	Value  int
-	PubKey string
+	Value      int
+	PubKeyHash []byte
 }
 
-// InputCanBeUnlock : function to check if the input of the transaction
-// comes from the correct owner
-func (in *TxInput) InputCanBeUnlock(data string) bool {
-	return in.Sig == data
+// UsesKey checks whether the given pubkeyhash matches the pubkey in TxInput
+func (in *TxInput) UsesKey(pubKeyHash []byte) bool {
+	lockingHash := wallet.PublicKeyHash(in.PubKey)
+
+	return bytes.Compare(lockingHash, pubKeyHash) == 0
 }
 
-// OutputCanBeUnlocked : function to check whether the reciever can retrieve
-// the transaction
-func (out *TxOutput) OutputCanBeUnlocked(key string) bool {
-	return out.PubKey == key
+// Lock converts the address to PubKeyHash to lock the output transaction
+func (out *TxOutput) Lock(address []byte) {
+	pubKeyHash := wallet.PublicKeyHash(address)
+	pubKeyHash = pubKeyHash[1 : len(pubKeyHash)-4]
+	out.PubKeyHash = pubKeyHash
+}
+
+// IsLockedWithKey checks whether the Output Transaction is locked with correct pubKeyHash
+func (out *TxOutput) IsLockedWithKey(pubKeyHash []byte) bool {
+	return bytes.Compare(out.PubKeyHash, pubKeyHash) == 0
+}
+
+// NewTxOutput creates new TxOutput by locking the transaction output
+// with pubkeyHash of the given address
+func NewTxOutput(value int, address string) *TxOutput {
+	txo := &TxOutput{value, nil}
+	txo.Lock([]byte(address))
+
+	return txo
 }
