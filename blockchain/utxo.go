b18898b8a90c1bae60dcf182d1a9280ecf017de6
Sadham-Hussian <sadhamhussian2020@gmail.com>
2020-07-22 23:24:03 +0530

    Add deleteprefix method to delete keys in database with prefix utxo-

--- blockchain/utxo.go
+++ blockchain/utxo.go
@@ -1,5 +1,11 @@
 package blockchain
 
+import (
+	"log"
+
+	"github.com/dgraph-io/badger"
+)
+
 var (
 	utxoPrefix   = []byte("utxo-")
 	prefixLength = len(utxoPrefix)
@@ -8,4 +14,51 @@ var (
 // UTXOSet struct to access blockchain database
 type UTXOSet struct {
 	BChain *Blockchain
+}
+
+// DeleteByPrefix deletes key in database with prefix utxoPrefix
+func (u *UTXOSet) DeleteByPrefix(prefix []byte) {
+	deleteKeys := func(keysForDelete [][]byte) error {
+		if err := u.BChain.Database.Update(func(txn *badger.Txn) error {
+			for _, key := range keysForDelete {
+				if err := txn.Delete(key); err != nil {
+					return err
+				}
+			}
+			return nil
+		}); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	collectSize := 10000
+	u.BChain.Database.View(func(txn *badger.Txn) error {
+		opts := badger.DefaultIteratorOptions
+		opts.PrefetchValues = false
+		it := txn.NewIterator(opts)
+		defer it.Close()
+
+		keysForDelete := make([][]byte, 0, collectSize)
+		keysCollected := 0
+		for it.Seek(prefix); it.ValidForPrefix(prefix); it.Next() {
+			key := it.Item().KeyCopy(nil)
+			keysForDelete = append(keysForDelete, key)
+			keysCollected++
+
+			if keysCollected == collectSize {
+				if err := deleteKeys(keysForDelete); err != nil {
+					log.Panic(err)
+				}
+				keysForDelete = make([][]byte, 0, collectSize)
+				keysCollected = 0
+			}
+		}
+		if keysCollected > 0 {
+			if err := deleteKeys(keysForDelete); err != nil {
+				log.Panic(err)
+			}
+		}
+		return nil
+	})
 }
